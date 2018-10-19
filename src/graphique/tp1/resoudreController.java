42f6fa3c2072d66610ec5fda43feb7c086c4d041
Riad Bensalem <fr_bensalem@esi.dz>
2018-10-19 22:50:56 +0200

    réalisqtion de lq premiere fenetre et la derniere

--- src/graphique/tp1/resoudreController.java
+++ src/graphique/tp1/resoudreController.java
@@ -1,7 +1,104 @@
 package graphique.tp1;
 
+import elements.tp1.Item;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import sample.Main;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
 /**
  * Created by Ryad on 19/10/2018.
  */
-public class resoudreController {
+public class resoudreController implements Initializable {
+    @FXML
+    private Button resoudre;
+    @FXML
+    private Label gainMax;
+    @FXML
+    private Label gainMaxtext;
+    @FXML
+    private ListView listeObj;
+    private ArrayList<Item> tabObj;
+    public void solve()
+    {
+        resoudre.setVisible(false);
+        int[][] solution=resoudre(tp1Controller.nbObjts,tp1Controller.poidsMax);
+        gainMax.setVisible(true);
+        gainMaxtext.setVisible(true);
+        gainMax.setText(Integer.toString(solution[tp1Controller.nbObjts+1][tp1Controller.poidsMax+1]));
+
+        tabObj=new ArrayList<Item>();
+
+        int tmp_j=tp1Controller.poidsMax;
+        ArrayList<Integer > list = new ArrayList< Integer >();
+        /*for(int k=tp1Controller.nbObjts;k > 0;k--){
+            if (solution[k][tmp_j]!=solution[k-1][tmp_j]){
+                list.add(tabObj[k-1].getNumero());
+                tmp_j=tmp_j-tabObj[k-1].getPoid();
+                System.out.print(" _"+tabObj[k-1].getPoid());
+            }
+
+        }*/
+        ObservableList<Integer> items = FXCollections.observableArrayList (list);
+        listeObj.setItems(items);
+        listeObj.setVisible(true);
+    }
+    public static int[][] resoudre(int nbObj,int w)
+    {
+        ArrayList<Item> listObj=new ArrayList<Item>();
+/*
+        for (int idx=0;idx<nbObj;idx++)
+        {
+            System.out.println("poid et gain de l objet "+idx+1);
+            listObj.add(new Item(scanner.nextLong(),scanner.nextLong()));
+        }
+*/
+        int ligne,colonne;
+        int[][] matrice=new int[nbObj+1][w+1];
+        for ( ligne=0;ligne<nbObj+1;ligne++)
+        {
+            for ( colonne=0;colonne<w+1;colonne++)
+            {
+                if ( (ligne==0) || (colonne==0) ) matrice[ligne][colonne]=0;
+                else
+                {
+                    if( colonne < listObj.get(ligne-1).getPoid() )
+                    {
+                        matrice[ligne][colonne]=matrice[ligne-1][colonne];
+
+                    }
+                    else
+                    {
+                        matrice[ligne][colonne]=max(matrice[ligne-1][colonne],
+                                matrice[ligne-1][colonne-( (int)listObj.get(ligne-1).getPoid() )]
+                                        + ( (int)listObj.get(ligne-1).getGain() ) );
+                    }
+                }
+            }
+        }
+        return matrice;
+    }
+    private static int max(int a, int b)
+    {
+        if (a>=b)
+            return a;
+        else
+            return b;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gainMax.setVisible(false);
+        gainMaxtext.setVisible(false);
+        listeObj.setVisible(false);
+    }
 }
--- src/graphique/tp1/tp1Controller.java
+++ src/graphique/tp1/tp1Controller.java
@@ -1,7 +1,31 @@
 package graphique.tp1;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import sample.Main;
+
+import java.io.IOException;
+import javafx.scene.control.TextField;
 /**
  * Created by Ryad on 19/10/2018.
  */
 public class tp1Controller {
+    public static int nbObjts;
+    public static int poidsMax;
+    @FXML
+    private TextField nbobj;
+    @FXML
+    private TextField poidMax;
+
+    public void next() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/graphique/tp1/remplirObjs.fxml"));
+        Main.myStage.setTitle("TP1");
+        Scene scene=new Scene(root, 487, 400);
+        nbObjts=Integer.parseInt(nbobj.getText());
+        poidsMax=Integer.parseInt(poidMax.getText());
+        Main.myStage.setScene(scene);
+        Main.myStage.show();
+    }
 }
